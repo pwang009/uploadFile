67af25317435ee8ecb99bed849b95f8e555b0f3f
Tony Wang <pwang009@gmail.com>
2024-06-09 22:39:07 -0700

    before collapse

--- src/app/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: any;
+}
 @Component({
   selector: 'app-custom-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatListModule, MatIconModule],
   templateUrl: './custom-sidenav.component.html',
   styleUrl: './custom-sidenav.component.scss'
 })
 export class CustomSidenavComponent {
-
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard', label: 'Dashboard', route: 'dashboard'
+    },
+    {
+      icon: 'comments', label: 'Comments', route: 'comments'
+    },
+    {
+      icon: 'analytics', label: 'Analytics', route: 'Analytics'
+    },
+    {
+      icon: 'settings', label: 'Settings', route: 'settings'
+    }
+  ]); 
 }
