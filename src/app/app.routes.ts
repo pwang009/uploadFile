01ee9c00d22bff2f3e06b50885947b36de09a9b9
Tony Wang <pwang009@gmail.com>
2024-06-15 22:11:13 -0700

    routing not work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+// import { DashboardComponent } from './components/pages/dashboard/dashboardComponent';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { CommentsComponent } from './components/pages/comments/comments.component';
+import { AnalyticsComponent } from './components/pages/analytics/analytics.component';
+import { SettingsComponent } from './components/pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'dashboard', },
+    { path: 'dashboard', component: DashboardComponent, },
+    { path: 'comments', component: CommentsComponent, },
+    { path: 'analytics', component: AnalyticsComponent, },
+    { path: 'settings', component: SettingsComponent, }
+];
--- src/app/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, Input, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -16,6 +16,11 @@ export type MenuItem = {
   styleUrl: './custom-sidenav.component.scss'
 })
 export class CustomSidenavComponent {
+
+  sideNavCollapsed = signal(false);
+  @Input() set collapsed(val: boolean){
+    this.sideNavCollapsed.set(val);
+  } 
   menuItems = signal<MenuItem[]>([
     {
       icon: 'dashboard', label: 'Dashboard', route: 'dashboard'
@@ -24,10 +29,12 @@ export class CustomSidenavComponent {
       icon: 'comments', label: 'Comments', route: 'comments'
     },
     {
-      icon: 'analytics', label: 'Analytics', route: 'Analytics'
+      icon: 'analytics', label: 'Analytics', route: 'analytics'
     },
     {
       icon: 'settings', label: 'Settings', route: 'settings'
     }
   ]); 
+
+  profilePicSize = computed(() => this.sideNavCollapsed() ? '32' : '100');
 }
