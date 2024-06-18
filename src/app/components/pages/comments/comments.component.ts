96a85ef0b7818503077b2c3d93d8dd48e2bfeaef
Tony Wang <pwang009@gmail.com>
2024-06-17 22:33:02 -0700

    upload file works

--- src/app/components/pages/comments/comments.component.ts
+++ src/app/components/pages/comments/comments.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FileUploadService } from '../../../services/upload-file.service';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss'
 })
-export class CommentsComponent {
+export class CommentsComponent implements OnInit {
+  currentFile?: File;
+  message = '';
+  fileInfos?: Observable<any>;
 
+  constructor(private uploadService: FileUploadService) { }
+
+  ngOnInit(): void {
+    this.fileInfos = this.uploadService.getFiles();
+  }
+  selectFile(event: any): void {
+    this.currentFile = event.target.files.item(0);
+  }
+
+  upload(): void {
+    if (this.currentFile) {
+      this.uploadService.upload(this.currentFile).subscribe({
+        next: (event: any) => {
+          if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            this.fileInfos = this.uploadService.getFiles();
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+
+          if (err.error && err.error.message) {
+            this.message = err.error.message;
+          } else {
+            this.message = 'Could not upload the file!';
+          }
+        },
+        complete: () => {
+          this.currentFile = undefined;
+        },
+      });
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {provideHttpClient} from '@angular/common/http';
 
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideHttpClient(),
     provideRouter(routes),
     importProvidersFrom(BrowserAnimationsModule)
   ]
